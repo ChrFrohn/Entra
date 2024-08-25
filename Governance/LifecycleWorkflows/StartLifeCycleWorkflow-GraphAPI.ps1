f5b6c7310b1cd48bfbbd2629eed1b2352fcb6aa0
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2024-08-25 12:20:38 +0200

    Update StartLifeCycleWorkflow-GraphAPI.ps1

--- Governance/LifecycleWorkflows/StartLifeCycleWorkflow-GraphAPI.ps1
+++ Governance/LifecycleWorkflows/StartLifeCycleWorkflow-GraphAPI.ps1
@@ -3,32 +3,35 @@ $LifeCycleWorkflowID = "" # ID of the lifecycle workflow
 
 # Service Principal authentication
 $ClientID = ""
-$TenantID = "" 
+$TenantID = ""
 $ClientSecret = ""
 
-$Body =  @{
-    GrantType    = "client_credentials"
-    Scope        = "https://graph.microsoft.com/.default"
-    ClientId     = $ClientID 
-    ClientSecret = $ClientSecret
+# Authenticate to Microsoft Graph
+$Body = @{
+    Grant_Type    = "client_credentials"
+    Scope         = "https://graph.microsoft.com/.default"
+    Client_Id     = $ClientID
+    Client_Secret = $ClientSecret
 }
- 
-$Connection = Invoke-RestMethod -Uri https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token -Method POST -Body $Body
 
+$Connection = Invoke-RestMethod `
+    -Uri "https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token" `
+    -Method POST `
+    -Body $Body
+
+# Define the URL and headers for activating the lifecycle workflow
+$Url = "https://graph.microsoft.com/v1.0/identityGovernance/lifecycleWorkflows/workflows/$LifeCycleWorkflowID/activate"
 $Headers = @{
-    "Authorization" = "Bearer " + $Connection.AccessToken
-    "Content-Type"  = "application/json"
+    "Content-Type" = "application/json"
+    "Authorization" = "Bearer " + $Connection.access_token
 }
 
-# URL to activate the Lifecycle Workflow
-$ActivateWorkflowURL = "https://graph.microsoft.com/v1.0/identityGovernance/lifecycleWorkflows/workflows/$LifeCycleWorkflowID/activate"
-
-# Body of the POST request
-$RequestBody = @{
-    Subjects = @(
-        @{ Id = $User }
+# Define the body of the request
+$Body = @{
+    "Subjects" = @(
+        @{ "Id" = $User }
     )
 } | ConvertTo-Json
 
-# The request to activate the workflow
-Invoke-RestMethod -Uri $ActivateWorkflowURL -Headers $Headers -Method Post -Body $RequestBody -ErrorAction Stop
+# Make the POST request to activate the lifecycle workflow
+Invoke-RestMethod -Uri $Url -Headers $Headers -Method Post -Body $Body -Verbose
