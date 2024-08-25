d5269cefc51cb87e11eaca09164db337833a58ba
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2024-08-25 11:26:52 +0200

    Create StartLifecycleWorkflow-Simple.ps1

---
+++ Governance/LifecycleWorkflows/StartLifecycleWorkflow-Simple.ps1
@@ -0,0 +1,15 @@
+# Import module and Connect to Microsoft Graph
+Import-Module Microsoft.Graph.Identity.Governance
+Connect-MgGraph -Scopes "IdentityGovernance.ReadWrite.ApprovalWorkflow"
+
+# Initialize Lifecycle Workflow
+$User = "" #Object ID or UPN of the user in Entra ID
+$LifeCycleWorkflowID = "" # ID of the lifecycle workflow
+
+$LifeCycleWorkflowParameters = @{
+    subjects = @{
+        id = $User # Entra ID users Object ID
+    }
+}
+
+Initialize-MgIdentityGovernanceLifecycleWorkflow -WorkflowId $LifeCycleWorkflowID -BodyParameter $LifeCycleWorkflowParameters
