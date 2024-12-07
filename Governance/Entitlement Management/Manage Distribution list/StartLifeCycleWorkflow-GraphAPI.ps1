2ab46dd1a4cd137b8e2f25d6f182cd1e63897f4d
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2024-12-07 18:51:13 +0100

    Create StartLifeCycleWorkflow-GraphAPI.ps1

---
+++ Governance/Entitlement Management/Manage Distribution list/StartLifeCycleWorkflow-GraphAPI.ps1
@@ -0,0 +1,41 @@
+param (
+    [Parameter (Mandatory = $true)] 
+    [string]$ObjectId
+)
+
+$LifeCycleWorkflowID = "" # ID of the lifecycle workflow
+
+# Service Principal authentication
+$ClientID = ""
+$TenantID = ""
+$ClientSecret = ""
+
+# Authenticate to Microsoft Graph
+$Body = @{
+    Grant_Type    = "client_credentials"
+    Scope         = "https://graph.microsoft.com/.default"
+    Client_Id     = $ClientID
+    Client_Secret = $ClientSecret
+}
+
+$Connection = Invoke-RestMethod `
+    -Uri "https://login.microsoftonline.com/$TenantID/oauth2/v2.0/token" `
+    -Method POST `
+    -Body $Body
+
+# Define the URL and headers for activating the lifecycle workflow
+$Url = "https://graph.microsoft.com/v1.0/identityGovernance/lifecycleWorkflows/workflows/$LifeCycleWorkflowID/activate"
+$Headers = @{
+    "Content-Type" = "application/json"
+    "Authorization" = "Bearer " + $Connection.access_token
+}
+
+# Define the body of the request
+$Body = @{
+    "Subjects" = @(
+        @{ "Id" = $ObjectId }
+    )
+} | ConvertTo-Json
+
+# Make the POST request to activate the lifecycle workflow
+Invoke-RestMethod -Uri $Url -Headers $Headers -Method Post -Body $Body -Verbose
