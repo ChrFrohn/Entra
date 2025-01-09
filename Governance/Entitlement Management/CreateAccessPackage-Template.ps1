0f027235cf708b4a64b51446ec19cf91d32dd154
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2025-01-09 11:28:16 +0100

    Update CreateAccessPackage-Template.ps1

--- Governance/Entitlement Management/CreateAccessPackage-Template.ps1
+++ Governance/Entitlement Management/CreateAccessPackage-Template.ps1
@@ -12,16 +12,16 @@ $AccessPackageCatalogId = "" # Sample: "00000000-0000-0000-0000-000000000000"
 # Request policy parameters
 $RequestPolicyName = "" # Sample: "Auto policy"
 $PolicyDescription = "" # Sample: "Auto policy for sales department"
-$membershipRule = "" # Sample: (user.department -eq "Sales")
+$membershipRule = "allMemberUsers" # "allMemberUsers", "specificAllowedTargets", "allConfiguredConnectedOrganizationUsers", "notSpecified"
+$Approver = "" # Object ID of the user in Entra ID / Change to groupId for group
 
 # Auto assignment policy parameters
 $AutoPolicyName = "" # Sample: "Auto policy"
 $AutoPolicyDescription = "" # Sample: "Auto policy for department X"
-$AutoAssignmentPolicyFilter = "" # Sample: (user.department -eq "Department X")
+$AutoAssignmentPolicyFilter = '' # Sample: '(user.department -eq "Department X")' 
 
 # Custom extension parameters
 $CustomExtensionId = "" # Sample: "00000000-0000-0000-0000-000000000000" - Needs to be created before running this script
-
 
 # Creating the access package
 
@@ -42,37 +42,48 @@ $NewAccessPackage = Get-MgEntitlementManagementAccessPackage -Filter "displayNam
 $RequestPolicyNameParameters = @{
 	displayName = $RequestPolicyName
 	description = $PolicyDescription
-	allowedTargetScope = "specificDirectoryUsers"
-	specificAllowedTargets = @(
-		@{
-			"@odata.type" = "#microsoft.graph.attributeRuleMembers"
-			description = $PolicyDescription
-			membershipRule = $membershipRule
-		}
-	)
-	automaticRequestSettings = @{
-		requestAccessForAllowedTargets = $true
-		removeAccessWhenTargetLeavesAllowedTargets = $true
-	}
-	accessPackage = @{
-		id = $NewAccessPackage.Id
-	}
-    customExtensionStageSettings = @(
-        @{
-            stage = "assignmentRequestGranted"
-            customExtension = @{
-                "@odata.type" = "#microsoft.graph.accessPackageAssignmentRequestWorkflowExtension"
-                id = $CustomExtensionId
-            }
-        }
-        @{
-            stage = "assignmentRequestRemoved"
-            customExtension = @{
-                "@odata.type" = "#microsoft.graph.accessPackageAssignmentRequestWorkflowExtension"
-                id = $CustomExtensionId
-            }
-        }
-    )
+    allowedTargetScope = $membershipRule
+ 
+    expiration = @{
+        type = "noExpiration"
+    }
+    requestorSettings = @{
+        enableTargetsToSelfAddAccess = $true
+        enableTargetsToSelfUpdateAccess = $true
+        enableTargetsToSelfRemoveAccess = $true
+        allowCustomAssignmentSchedule = $true
+        enableOnBehalfRequestorsToAddAccess = $false
+        enableOnBehalfRequestorsToUpdateAccess = $false
+        enableOnBehalfRequestorsToRemoveAccess = $false
+        onBehalfRequestors = @(
+        )
+    }
+	requestApprovalSettings = @{
+		isApprovalRequiredForAdd = "true"
+		isApprovalRequiredForUpdate = "true"
+		stages = @(
+			@{
+				durationBeforeAutomaticDenial = "P7D" # 7 days
+				isApproverJustificationRequired = "false"
+				isEscalationEnabled = "false" 
+				fallbackPrimaryApprovers = @(
+				)
+				escalationApprovers = @(
+				)
+				fallbackEscalationApprovers = @(
+				)
+				primaryApprovers = @(
+					@{
+						"@odata.type" = "#microsoft.graph.singleUser" # For group use: "@odata.type" = "#microsoft.graph.groupMembers"
+						userId = $Approver # Object ID of the user in Entra ID / Change to groupId for group
+					}
+				)
+			}
+		)
+    }
+    accessPackage = @{
+        id = $NewAccessPackage.Id
+    }
 }
 
 New-MgEntitlementManagementAssignmentPolicy -BodyParameter $RequestPolicyNameParameters 
