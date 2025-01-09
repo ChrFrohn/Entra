b8845025a48cfe8033d9dea7218fe87e54e8024f
Christian Frohn <50671696+ChrFrohn@users.noreply.github.com>
2025-01-09 12:10:40 +0100

    Update AddCustomExtensionParamterToPolicy.ps1

--- Governance/Entitlement Management/AddCustomExtensionParamterToPolicy.ps1
+++ Governance/Entitlement Management/AddCustomExtensionParamterToPolicy.ps1
@@ -1,3 +1,6 @@
+# Custom extension parameters
+$CustomExtensionId = "" # Sample: "00000000-0000-0000-0000-000000000000" - Needs to be created before running this script
+
 # Add this part to the parameters of a new policy in Entra ID Goverance Entitlement Management
 
 customExtensionStageSettings = @(
