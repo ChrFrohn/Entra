9f45b76ebb5291300d3400d00bb18f071cf26ec2
ChrFrohn <frohn@outlook.com>
2025-01-30 14:23:29 +0100

    Update AddApplicationToAccessPackage.ps1 for clarity in variable naming and enhance parameter structure

--- Governance/Entitlement Management/AddApplicationToAccessPackage.ps1
+++ Governance/Entitlement Management/AddApplicationToAccessPackage.ps1
@@ -1,19 +1,20 @@
 # Add an application as a resource to an Access Package in Entitlement Management
 $CatalogId = "" # ID of the catalog - Sample: "db4859bf-43c3-49fa-ab13-8036bd333ebe"
-$ApplicationId = "" # ID of the application - Sample: "db4859bf-43c3-49fa-ab13-8036bd333ebe"
+$ApplicationId = "" # Object ID of the Enterprise Application - Sample: "db4859bf-43c3-49fa-ab13-8036bd333ebe"
 $AccessPackageId = "" # ID of the Acceess package - Sample: "db4859bf-43c3-49fa-ab13-8036bd333ebe"
 
-# Add the application as a resource to the Catalog
-$ApplicationResourceAddParameters = @{
-  requestType = "adminAdd"
-  resource = @{
-    originId = $ApplicationId
-    originSystem = "AadApplication"
-  }
-  catalog = @{ id = $CatalogId }
+$ApplicationParameters = @{
+	requestType = "adminAdd"
+	resource = @{
+		originId = $ApplicationId
+		originSystem = "AadApplication"
+	}
+	catalog = @{
+		id = $CatalogId
+	}
 }
 
-New-MgEntitlementManagementResourceRequest -BodyParameter $ApplicationResourceAddParameters
+New-MgEntitlementManagementResourceRequest -BodyParameter $ApplicationParameters
 
 # Get the Application as a resource from the Catalog
 $CatalogResources = Get-MgEntitlementManagementCatalogResource -AccessPackageCatalogId $CatalogId -ExpandProperty "scopes" -All
@@ -24,7 +25,7 @@ $ApplicationResourceScope = $ApplicationResource.Scopes[0]
 # Add the Application as a resource role to the Access Package
 $ApplicationResourceFilter = "(originSystem eq 'AadApplication' and resource/id eq '" + $ApplicationResourceId + "')"
 $ApplicationResourceRoles = Get-MgEntitlementManagementCatalogResourceRole -AccessPackageCatalogId $CatalogId -Filter $ApplicationResourceFilter -All -ExpandProperty "resource"
-$ApplicationMemberRole = $ApplicationResourceRoles | Where-Object { $_.DisplayName -eq $servicePrincipalRoleName }
+$ApplicationMemberRole = $ApplicationResourceRoles | Where-Object { $_.DisplayName -eq "Default Access" }
 
 $ApplicationResourceRoleScopeParameters = @{
   role = @{
