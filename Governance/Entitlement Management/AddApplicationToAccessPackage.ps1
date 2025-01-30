49036eff7bfc721f7372ede2ebd51dc307eebd41
ChrFrohn <frohn@outlook.com>
2025-01-30 13:34:58 +0100

    Refactor AddApplicationToAccessPackage.ps1 to improve variable declarations and enhance comment clarity

--- Governance/Entitlement Management/AddApplicationToAccessPackage.ps1
+++ Governance/Entitlement Management/AddApplicationToAccessPackage.ps1
@@ -1,13 +1,7 @@
-Import-Module Microsoft.Graph.Identity.Governance
-
-Connect-MgGraph -Scopes "EntitlementManagement.ReadWrite.All"
-
 # Application that needs to be added as a resource to the Catalog and Access Package
-$ApplicationId = ""
-
-# Catalog and Access package values
-$CatalogId = ""
-$AccessPackageId = ""
+$CatalogId = "" # ID of the catalog - Sample: "db4859bf-43c3-49fa-ab13-8036bd333ebe"
+$ApplicationId = "" # ID of the application - Sample: "db4859bf-43c3-49fa-ab13-8036bd333ebe"
+$AccessPackageId = "" # ID of the Acceess package - Sample: "db4859bf-43c3-49fa-ab13-8036bd333ebe"
 
 # Add the application as a resource to the Catalog
 $ApplicationResourceAddParameters = @{
@@ -21,13 +15,13 @@ $ApplicationResourceAddParameters = @{
 
 New-MgEntitlementManagementResourceRequest -BodyParameter $ApplicationResourceAddParameters
 
-# Get the application as a resource from the Catalog
+# Get the Application as a resource from the Catalog
 $CatalogResources = Get-MgEntitlementManagementCatalogResource -AccessPackageCatalogId $CatalogId -ExpandProperty "scopes" -All
 $ApplicationResource = $CatalogResources | Where-Object { $_.OriginId -eq $ApplicationId }
 $ApplicationResourceId = $ApplicationResource.id
 $ApplicationResourceScope = $ApplicationResource.Scopes[0]
 
-# Add the application as a resource role to the Access Package
+# Add the Application as a resource role to the Access Package
 $ApplicationResourceFilter = "(originSystem eq 'AadApplication' and resource/id eq '" + $ApplicationResourceId + "')"
 $ApplicationResourceRoles = Get-MgEntitlementManagementCatalogResourceRole -AccessPackageCatalogId $CatalogId -Filter $ApplicationResourceFilter -All -ExpandProperty "resource"
 $ApplicationMemberRole = $ApplicationResourceRoles | Where-Object { $_.DisplayName -eq $servicePrincipalRoleName }
