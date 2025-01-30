73ca8c2942304f4e4132c2f7ad149de13e603200
ChrFrohn <frohn@outlook.com>
2025-01-30 13:28:32 +0100

    Refactor AddGroupToCatalog.ps1 to streamline variable declarations for Catalog and Group Object IDs

--- Governance/Entitlement Management/AddGroupToCatalog.ps1
+++ Governance/Entitlement Management/AddGroupToCatalog.ps1
@@ -1,12 +1,8 @@
-Connect-MgGraph -Scopes "EntitlementManagement.ReadWrite.All"
+# Catalog Id and Group Object Id for the Catalog where the Group needs to be added
+$CatalogId = "" # Catalog ID - Sample: "db4859bf-43c3-49fa-ab13-8036bd333ebe"
+$GroupObjectId  = "" # Object ID of the group - Sample: "b3b3b3b3-3b3b-3b3b-3b3b-3b3b3b3b3b3b"
 
-# Group that needs to be added as a resource to the Catalog
-$GroupObjectId  = "" # Object ID of the group
-
-# Catalog
-$CatalogId = ""
-
-# Add the group as a resource to the Catalog
+# Add the Group as a resource to the Catalog
 $GroupResourceAddParameters = @{
   requestType = "adminAdd"
   resource = @{
