fb8385507ce5aa56fd655185c1852ab6319f9092
ChrFrohn <frohn@outlook.com>
2025-01-30 10:06:07 +0100

    Add comment to GetSpecificCatalog.ps1 script for clarity on functionality

--- Governance/Entitlement Management/GetSpecificCatalog.ps1
+++ Governance/Entitlement Management/GetSpecificCatalog.ps1
@@ -1,3 +1,4 @@
+# Get a specific Catalog by name from Entra ID Governance Entitlement Management
 $CatalogName = "General"
 
 Get-MgEntitlementManagementCatalog -Filter "displayName eq '$CatalogName'" -All
