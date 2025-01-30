76cd0cb769068ae77b440ab7d5bc59df3596da34
ChrFrohn <frohn@outlook.com>
2025-01-30 12:50:44 +0100

    Add PowerShell script to add SharePoint Online site to access package

---
+++ Governance/Entitlement Management/AddSharePointOnlineSiteToAccessPackage.ps1
@@ -0,0 +1,48 @@
+# Import the necessary module
+Import-Module Microsoft.Graph.Identity.Governance
+
+# Connect to Microsoft Graph
+Connect-MgGraph -Scopes "EntitlementManagement.ReadWrite.All"
+
+# Define the SharePoint Online site details
+$SharePointSiteURL = ""
+$CatalogId = ""  # 
+$SharePointSiteRole = "4" # 4 = Vistors / 5 = Members / 3 = Owners | Run this to get IDs
+
+# Add the SharePoint Online site as a resource to the catalog
+$SharePointResourceAddParameters = @{
+  requestType = "adminAdd"
+  resource = @{
+    originId = $SharePointSiteURL
+    originSystem = "SharePointOnline"
+  }
+  catalog = @{ id = $CatalogId }
+}
+
+New-MgEntitlementManagementResourceRequest -BodyParameter $SharePointResourceAddParameters
+
+# Get the SharePoint Online site resource ID
+$CatalogResources = Get-MgEntitlementManagementCatalogResource -AccessPackageCatalogId $CatalogId -ExpandProperty "scopes" -All
+$SharePointResource = $CatalogResources | Where-Object { $_.OriginId -eq $SharePointSiteURL }
+$SharePointResourceId = $SharePointResource.id
+
+#  Create a new access package resource role scope for the SharePoint Online site
+$params = @{
+  role = @{
+    displayName = "Contributors"
+    originSystem = "SharePointOnline"
+    originId = $SharePointSiteRole
+    resource = @{
+      id = $SharePointResourceId
+    }
+  }
+  scope = @{
+    displayName = "Root"
+    description = "Root Scope"
+    originId = $SharePointSiteURL
+    originSystem = "SharePointOnline"
+    isRootScope = $true
+  }
+}
+
+New-MgEntitlementManagementAccessPackageResourceRoleScope -AccessPackageId "db4859bf-43c3-49fa-ab13-8036bd333ebe" -BodyParameter $params
