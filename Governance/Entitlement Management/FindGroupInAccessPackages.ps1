2cee92182b9f5b30da44f7fdcca9171478e67ca0
ChrFrohn <frohn@outlook.com>
2025-05-07 13:25:24 +0200

    Add script to search for groups in Entra ID and list associated access packages

---
+++ Governance/Entitlement Management/FindGroupInAccessPackages.ps1
@@ -0,0 +1,35 @@
+# Connect to Microsoft Graph
+Connect-MgGraph -Scopes "Group.Read.All", "EntitlementManagement.Read.All"
+
+# Search for the group in Entra ID
+$targetGroup = "" # Name of the group to search for
+$group = Get-MgGroup -Filter "displayName eq '$targetGroup'"
+
+if (-not $group) 
+{
+    Write-Host "Group '$targetGroup' not found in Entra ID." -ForegroundColor Red
+    return
+}
+
+# ObjectId of the group
+$targetGroupId = $group.Id
+
+# Get all access packages
+$accessPackages = Get-MgEntitlementManagementAccessPackage
+
+Write-Host "Searching for access packages containing group '$targetGroup'..."
+
+foreach ($package in $accessPackages) 
+{
+    $packageDetails = Get-MgEntitlementManagementAccessPackage -AccessPackageId $package.Id -ExpandProperty "resourceRoleScopes(`$expand=role,scope)"
+       
+    foreach ($scope in $packageDetails.ResourceRoleScopes.Scope) 
+    {
+        if ($scope.OriginId -eq $targetGroupId) 
+        {
+            Write-Host "Found: Group '$targetGroup' is in Access Package: $($package.DisplayName)" -ForegroundColor Green
+        }
+    }
+}
+
+Write-Host "Search complete."
