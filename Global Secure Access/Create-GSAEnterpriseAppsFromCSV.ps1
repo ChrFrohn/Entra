b2363332e4b6def7fbab3cbec71ad543b42a52cd
Christian Frohn <cfp@christianfrohn.dk>
2025-10-20 22:18:44 +0200

    Update CSV file path and correct variable name for application prefix

--- Global Secure Access/Create-GSAEnterpriseAppsFromCSV.ps1
+++ Global Secure Access/Create-GSAEnterpriseAppsFromCSV.ps1
@@ -1,12 +1,12 @@
 Connect-Entra -Scopes 'NetworkAccessPolicy.ReadWrite.All', 'Application.ReadWrite.All', 'NetworkAccess.ReadWrite.All', 'AppRoleAssignment.ReadWrite.All', 'Group.ReadWrite.All', 'Group.Create'
 
 # CSV infomation
-$CsvFilePath = "C:\path\to\your\file.csv" # Update with actual path to the CSV file
+$CsvFilePath = "C:\Users\ChristianFrohn\GitHub\Entra\Global Secure Access\WebApplications-Sample.csv" # Update with actual path to the CSV file
 $CsvFileContent = Import-Csv $CsvFilePath -Delimiter ","
 
 # Application and group naming
 $ConnectorGroupName = "YourConnectorGroupName" # Update with actual connector group name
-$ApplicationPrefix = "GSA - Web -" # Update with desired application prefix
+$AppPrefix = "GSA - Web -" # Update with desired application prefix
 $SecurityGroupPrefix = "GSA - Web -" # Update with desired security group prefix
 
 # Enterprise Application settings
